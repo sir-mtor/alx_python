635d69072a36a8bcc4d733c18cba5cf122a5c5e8
sir <gari69wat@gmail.com>
2023-12-05 14:15:30 +0000

    printing from py

---
+++ hello-python_world/5-print_comb2.py
@@ -0,0 +1,2 @@
+for i in range(99):
+    print("Decimal: {}, Hexadecimal: {}".format(i, hex(i)))
---
+++ hello-python_world/6-print_comb3.py
@@ -0,0 +1,4 @@
+for i in range(10):
+    for j in range(i + 1, 10):
+        print("{:02d}, ".format(i * 10 + j), end="")
+print()
