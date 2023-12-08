ab05fa5a1a147d1364227efe52d37af5eecdca4a
sir <gari69wat@gmail.com>
2023-12-08 22:05:16 +0000

    string

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,18 +1,9 @@
-def reverse_string(string):
-    """
-    Reverses a given string.
 
-    Parameters:
-    string (str): The input string.
+def reverse_string(input_string):
+    return input_string[::-1]
 
-    Returns:
-    str: The reversed string.
-    """
+
+# def reverse_string(string):
+   
     reversed_str = string[::-1]
     return reversed_str
-
-# Example usage:
-input_str = "Hello, World!"
-reversed_result = reverse_string(input_str)
-print(f"The reverse of '{input_str}' is: '{reversed_result}'")
-
