3b54c375a77b67a393bd52eebf2977965e599c87
sir <gari69wat@gmail.com>
2023-12-08 22:18:18 +0000

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,18 +1,6 @@
-
 # File: add_0.py
-# def add(a, b):
-#    return a + b
 
-# Main program
-a = 1
-b = 2
+def add(a, b):
+    return a + b
 
-# Do not use * for importing or __import__
-add_module = __import__('add_0')
-
-# Use variables a and b as arguments when calling the function
-result = add_module.add(a, b)
-
-# Display the result using print function with string format
-print("{} + {} = {}".format(a, b, result))
 
