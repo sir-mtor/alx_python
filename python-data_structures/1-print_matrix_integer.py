34fb6dc9e31966451e63afdcad3e6ec0c9e50a6f
sir <gari69wat@gmail.com>
2023-12-08 17:29:30 +0000

    c

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,7 +1,19 @@
 def print_matrix_integer(matrix):
     for row in matrix:
-        for element in row:
-            print("{:d}".format(element), end=" ")
+        for i, element in enumerate(row):
+            if i == len(row) - 1:
+                # Last element in the row, no trailing space
+                print("{:d}".format(element), end="")
+            else:
+                # Not the last element in the row, print with trailing space
+                print("{:d} ".format(element), end="")
         print()
 
+# Example usage:
+matrix_example = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
+print_matrix_integer(matrix_example)
