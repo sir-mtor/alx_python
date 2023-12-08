483ae980740293900038d9daa53e9c3cda144788
sir <gari69wat@gmail.com>
2023-12-08 11:19:00 +0000

    matrix

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,14 @@
+def print_matrix_integer(matrix):
+    for row in matrix:
+        for element in row:
+            print("{:d}".format(element), end=" ")
+        print()
+
+# Example usage:
+matrix_example = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix_example)
