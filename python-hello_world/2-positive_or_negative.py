a58859de69c3f02c72abb1c05233c3f0879b9b43
sir <gari69wat@gmail.com>
2023-12-09 10:44:08 +0000

    changes

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+print("The number is:", number)
+print("is positive" if number > 0 else "is zero" if number == 0 else "is negative")
