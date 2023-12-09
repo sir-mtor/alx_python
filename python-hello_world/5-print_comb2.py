d7a647951776b0db1d5b7856077ddb13ab527610
sir <gari69wat@gmail.com>
2023-12-09 14:34:07 +0000

    hexa

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,5 +1,5 @@
 
 for i in range(100):
     print(f"{i:02d}", end=", " if i < 99 else "\n")
-for i in range(100):
-    print(f"{i:02d}", end=", " if i < 99 else "\n")
+for i in range(99):
+    print("Decimal: {}, Hexadecimal: {}".format(i, hex(i)))
