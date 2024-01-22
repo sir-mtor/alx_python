3ddb6075dd8cb4f98e4ce4bf8e59c423325c3bbd
sir <gari69wat@gmail.com>
2024-01-22 18:53:31 +0000

    2raise

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,3 +1,4 @@
+'''
 def raise_exception_msg(message=""):
     try:
         # Raise a name exception with the specified message
@@ -12,3 +13,14 @@ def raise_exception_msg(message=""):
 # Example usage:
 custom_message = "This is a custom message."
 raise_exception_msg(custom_message)
+'''
+def raise_exception_msg(message=""):
+    raise NameError(message)
+
+# Test the function
+if __name__ == "__main__":
+    try:
+        raise_exception_msg("C is fun")
+    except NameError as ne:
+        print(ne)
+
