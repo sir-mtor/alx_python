0dfbf537f0bc05ed12d76a8a5baf0b6fa3549eee
sir <gari69wat@gmail.com>
2024-01-22 19:10:16 +0000

    arg

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     num_args = len(argv) - 1
     args_list = argv[1:]
 
-    print("{} argument{}{}.".format(
+    print("{} argument{}{}".format(
         num_args,
         's' if num_args != 1 else '',
         ':' if num_args != 0 else '.'
