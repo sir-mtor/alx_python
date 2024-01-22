c71beb6b1500e68fd43a723218b3460be01e0a4e
sir <gari69wat@gmail.com>
2024-01-22 19:15:43 +0000

    las

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,7 +3,7 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+last_digit = number % 10
 
 print("Last digit of {} is {} and is ".format(number, last_digit), end="")
 
