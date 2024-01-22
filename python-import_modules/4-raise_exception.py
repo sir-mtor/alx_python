13bb226629f5234a0d91014f9f2327121fdabbeb
sir <gari69wat@gmail.com>
2024-01-22 18:51:38 +0000

    raise

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,18 +1,3 @@
-''''
-def raise_exception():
-    try:
-        # Raise a type exception by attempting to convert a string to an integer
-        value = int("abc")
-    except TypeError as e:
-        print("Type error occurred:", e)
-    else:
-        print("No exception occurred.")
-    finally:
-        print("Finally block executed.")
-
-# Example usage:
-raise_exception()
-'''
 def raise_exception():
     raise TypeError("Exception raised")
 
