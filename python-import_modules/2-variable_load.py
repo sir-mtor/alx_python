09fedd67ba6fbb3f63f63aa0bd28a61a71866717
sir <gari69wat@gmail.com>
2024-01-22 18:41:27 +0000

    var

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,14 +1,3 @@
-'''''
-# main.py
-import variable_load_2
-# You are not allowed to use * for importing or __import__
-from variable_load_2 import a
-
-# Your code should not be executed when imported
-if __name__ == "__main__":
-    print("The value of 'a' is:", a)
-'''
-  #!/usr/bin/python3
 from variable_load_2 import a
 
 print(a)
