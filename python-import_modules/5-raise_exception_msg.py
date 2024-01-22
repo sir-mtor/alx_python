3e9234e4b370bb50e6f104d2cd99243a022d2341
sir <gari69wat@gmail.com>
2024-01-22 18:54:24 +0000

    4 raise

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,19 +1,3 @@
-'''
-def raise_exception_msg(message=""):
-    try:
-        # Raise a name exception with the specified message
-        raise NameError(message)
-    except NameError as e:
-        print("Name error occurred:", e)
-    else:
-        print("No exception occurred.")
-    finally:
-        print("Finally block executed.")
-
-# Example usage:
-custom_message = "This is a custom message."
-raise_exception_msg(custom_message)
-'''
 def raise_exception_msg(message=""):
     raise NameError(message)
 
@@ -23,4 +7,3 @@ if __name__ == "__main__":
         raise_exception_msg("C is fun")
     except NameError as ne:
         print(ne)
-
