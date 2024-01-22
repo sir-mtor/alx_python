dd5d6797c9fdbbf84a0043dcba8e8b7d47a5cb03
sir <gari69wat@gmail.com>
2024-01-22 18:30:54 +0000

    arg

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,3 +1,4 @@
+''''
 # Main program
 if __name__ == "__main__":
     import sys
@@ -19,3 +20,18 @@ if __name__ == "__main__":
     # Print the list of arguments with their positions
     for i in range(1, num_args + 1):
         print("{}: {}".format(i, sys.argv[i]), end="\n")
+'''''
+#!/usr/bin/python3
+from sys import argv
+
+num_args = len(argv) - 1
+
+if num_args == 0:
+    print("0 arguments.")
+elif num_args == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(num_args))
+
+for i in range(1, num_args + 1):
+    print("{}: {}".format(i, argv[i]))
