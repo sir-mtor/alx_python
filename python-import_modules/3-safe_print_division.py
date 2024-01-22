b2408f2d0d6dd9869c4812329650b53a3ff53364
sir <gari69wat@gmail.com>
2024-01-22 18:47:19 +0000

    safe

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,3 +1,4 @@
+''''
 def safe_print_division(a, b):
     try:
         result = a / b
@@ -12,7 +13,28 @@ def safe_print_division(a, b):
         return result
     finally:
         print("Finally block executed.")
-
 a = 10
 b = 2
 safe_print_division(a, b)
+'''
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
+
+# Test the function
+if __name__ == "__main__":
+    a = 12
+    b = 2
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
+
+    a = 12
+    b = 0
+    result = safe_print_division(a, b)
+    print("{:d} / {:d} = {}".format(a, b, result))
