55cade999876a70ef3322117276e67bcb757dd7b
sir <gari69wat@gmail.com>
2024-01-23 14:57:28 +0000

    xx

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,23 +1,3 @@
-'''
-def square_matrix_simple(matrix=[]):
-    new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            new_matrix[i][j] = matrix[i][j] ** 2
-
-    return new_matrix
-
-# Example usage:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
-'''
 def square_matrix_simple(matrix=[]):
     # Create a new matrix with the same size as the input matrix
     new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,3 +1,4 @@
+'''
 def common_elements(set_1, set_2):
     # Initialize an empty set to store common elements
     common_set = set()
@@ -17,3 +18,14 @@ c_set = common_elements(set_1, set_2)
 
 result = common_elements(set_1, set_2)
 print(result)
+'''
+def common_elements(set_1, set_2):
+    # Use the intersection operation to find common elements between the two sets
+    common_set = set_1 & set_2
+    return common_set
+
+# Example usage:
+set_1 = {"Python", "C", "Javascript"}
+set_2 = {"Bash", "C", "Ruby", "Perl"}
+c_set = common_elements(set_1, set_2)
+print(sorted(list(c_set)))
