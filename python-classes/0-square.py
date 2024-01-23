cc6e0aa4cd1ec0dcf7d531a528cd08e1f2147325
sir <gari69wat@gmail.com>
2024-01-23 15:52:03 +0000

    docu

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,18 +1,38 @@
-'''
 class Square:
+    """
+    This class defines a square by a private instance attribute: size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size): Initializes a new instance of the Square class.
+    """
+
     def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+            size (int): The size of the square.
+        """
         self.__size = size
 
-    
+# Test the implementation
+if __name__ == "__main__":
+    my_square = Square(3)
 
-    def get_size(self):
-        return self.__size
+    # Print type and dictionary representation of the object
+    print(type(my_square))
+    print(my_square.__dict__)
 
-    def set_size(self, size):
-        self.__size = size
-'''
-class Square:
-    def __init__(self, size):
-        self.__size = size
-def area(self):
-        return self.__size ** 2
+    # Try accessing size and __size (should raise AttributeError)
+    try:
+        print(my_square.size)
+    except AttributeError as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except AttributeError as e:
+        print(e)
