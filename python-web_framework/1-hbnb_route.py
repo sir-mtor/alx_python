cef0124c47e50fd1fe125067f286ff0704cb2896
sir <gari69wat@gmail.com>
2024-01-24 15:57:20 +0000

    hbnb

--- python-web_framework/1-hbnb_route.py
+++ python-web_framework/1-hbnb_route.py
@@ -1,3 +1,18 @@
+"""
+1-hbnb_route.py
+----------------
+
+A simple Flask web application that displays "Hello HBNB!".
+
+Usage:
+    python3 1-hbnb_route.py
+
+Routes:
+    /: Display "Hello HBNB!"
+
+Note: The application listens on 0.0.0.0, port 5000.
+      Use the option strict_slashes=False in the route definition.
+"""
 from flask import Flask
 
 app = Flask(__name__)
