4465fc82079e4f6aeb77f9efeb44d0a98072dc91
sir <gari69wat@gmail.com>
2024-01-25 15:29:30 +0000

    name

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,38 +1,32 @@
 import MySQLdb
 import sys
 
-def filter_states_by_name_starting_with_N(username, password, database):
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    # Create a cursor object to execute SQL queries
+    # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query to retrieve states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
+    # Execute the SQL query to fetch states based on the provided state name
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
     cursor.execute(query)
 
     # Fetch all the rows
-    states = cursor.fetchall()
+    rows = cursor.fetchall()
 
-    # Display the results as in the example
-    for state in states:
-        print(state)
+    # Print the results
+    for row in rows:
+        print(row)
 
     # Close the cursor and database connection
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get MySQL credentials from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Call the function to filter and list states
-    filter_states_by_name_starting_with_N(mysql_username, mysql_password, database_name)
