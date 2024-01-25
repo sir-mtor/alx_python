9cfcd3b296303f54da8db95c80013f321d46d670
sir <gari69wat@gmail.com>
2024-01-25 17:56:58 +0000

    nc

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,3 +1,4 @@
+'''
 import MySQLdb
 import sys
 
@@ -31,6 +32,7 @@ if __name__ == "__main__":
     db.close()
 
     '''
+'''
 import MySQLdb
 import sys
 connect = MySQLdb.connect(
@@ -51,3 +53,35 @@ for obj in cursor:
 cursor.close()
 connect.close()
 '''
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        sys.exit(1)
+
+    # Get command line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+    # Create a cursor object
+    cursor = db.cursor()
+
+    # Execute the SQL query to fetch all records where name is 'Nevada'
+    query = "SELECT * FROM states WHERE name = 'Nevada' ORDER BY id ASC;"
+    cursor.execute(query)
+
+    # Fetch all the rows
+    rows = cursor.fetchall()
+
+    # Print the results
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
