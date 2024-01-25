dcd1e2b39656ff8fd417628396bd927b4148c13f
sir <gari69wat@gmail.com>
2024-01-25 17:04:44 +0000

    command

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,45 +1,3 @@
-'''
-import MySQLdb
-import sys
-
-def filter_states_by_name_starting_with_N(username, password, database):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object to execute SQL queries
-    cursor = db.cursor()
-
-    # Execute the SQL query to retrieve states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC"
-    cursor.execute(query)
-
-    # Fetch all the rows
-    states = cursor.fetchall()
-
-    # Display the results as in the example
-    for state in states:
-        print(state)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
-
-if __name__ == "__main__":
-    # Check if the correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Get MySQL credentials from command line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    # Call the function to filter and list states
-    filter_states_by_name_starting_with_N(mysql_username, mysql_password, database_name)
-'''
-
-#!/usr/bin/python3
 import MySQLdb
 import sys
 
@@ -58,11 +16,8 @@ if __name__ == "__main__":
     # Create a cursor object
     cursor = db.cursor()
 
-    # Get the state name to search
-    state_name = input("Enter state name to search: ")
-
-    # Execute the SQL query to fetch states based on the provided state name
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
+    # Execute the SQL query to fetch states where name is 'Nevada'
+    query = "SELECT * FROM states WHERE name = 'Nevada' ORDER BY id ASC;"
     cursor.execute(query)
 
     # Fetch all the rows
