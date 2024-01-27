185a6f1e5dd8a03b8239d4f591ce8a4011647faf
sir <gari69wat@gmail.com>
2024-01-27 13:08:58 +0000

    char

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,3 +1,4 @@
+'''
 import MySQLdb
 import sys
 
@@ -31,6 +32,44 @@ if __name__ == "__main__":
     # Print the results
     for row in results:
         print(row)
+
+    # Close the database connection
+    db.close()
+'''
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to retrieve states starting with 'N'
+    cursor.execute("SELECT name FROM states WHERE name LIKE 'N%' OR name LIKE 'n%' ORDER BY id ASC")
+
+    # Fetch all the rows in a list of tuples
+    results = cursor.fetchall()
+
+    # Print the state names
+    for row in results:
+        print(row[0])
 
     # Close the database connection
     db.close()
