f8bf91d58ecf281f9b7e9da03c301bcb9762b74c
sir <gari69wat@gmail.com>
2024-01-27 13:47:16 +0000

    mar

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,22 +1,3 @@
-'''
-def square_matrix_simple(matrix=[]): 
-    new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-
-    # Iterate over the rows and columns of the input matrix
-    for i in range(len(matrix)):
-        for j in range(len(matrix[i])):
-            # Square each element and store it in the corresponding position of the new matrix
-            new_matrix[i][j] = matrix[i][j] ** 2
-
-    return new_matrix
-
-# Example:
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-'''
 def square_matrix_simple(matrix=[]):
     new_matrix = []
     for row in matrix:
@@ -26,7 +7,6 @@ def square_matrix_simple(matrix=[]):
         new_matrix.append(new_row)
     return new_matrix
 
-# Test the function
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
@@ -34,4 +14,4 @@ matrix = [
 ]
 new_matrix = square_matrix_simple(matrix)
 print(new_matrix)
-print(matrix)  # Original matrix remains unchanged
+print(matrix)  
