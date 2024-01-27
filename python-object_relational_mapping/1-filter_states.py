f43ec04276f4f5cedcccd39434aaecca309228f9
sir <gari69wat@gmail.com>
2024-01-27 12:59:26 +0000

    cc

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,37 +1,3 @@
-
-'''
-import MySQLdb
-import sys 
-
-def filter_states_by_name_starting_with_N(username, password, database):
-    
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name LIKE 'N%'OR name LIKE 'n%' ORDER BY states.id ASC"
-    cursor.execute(query)
-
-    states = cursor.fetchall()
-
-    for state in states:
-        print(state)
-
-    cursor.close()
-    db.close()
-
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-
-    filter_states_by_name_starting_with_N(mysql_username, mysql_password, database_name)
-'''
-
 import MySQLdb
 import sys
 
@@ -43,7 +9,7 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-
+'''
     # Connect to MySQL server
     db = MySQLdb.connect(
         host="localhost",
@@ -52,7 +18,7 @@ if __name__ == "__main__":
         passwd=password,
         db=database
     )
-
+'''
     # Create a cursor object using cursor() method
     cursor = db.cursor()
 
