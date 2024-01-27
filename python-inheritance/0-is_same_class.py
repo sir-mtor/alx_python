301d76288d5d7f96136186d45eea17f178084d39
sir <gari69wat@gmail.com>
2024-01-27 17:07:16 +0000

    module

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,15 +1,3 @@
-'''
-def is_same_class(obj, a_class):
-    return type(obj) == a_class
-
-obj_instance = a()
-result = is_same_class(obj_instance, ExampleClass)
-print(result)  # Output: True
-
-another_instance = "Not a class instance"
-result = is_same_class(another_instance, ExampleClass)
-print(result)  # Output: False
-'''
 """Module for checking if an object is exactly an instance of a specified class."""
 
 def is_same_class(obj, a_class):
