7eb79357f8f5526cedd24f38625aab8982c92987
sir <gari69wat@gmail.com>
2024-01-27 13:19:51 +0000

    new

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -52,7 +52,7 @@ for obj in cursor:
     print(obj)
 cursor.close()
 connect.close()
-'''
+
 #!/usr/bin/python3
 import MySQLdb
 import sys
@@ -84,4 +84,43 @@ if __name__ == "__main__":
 
     # Close the cursor and database connection
     cursor.close()
+    db.close()
+'''
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Execute SQL query to retrieve states matching the provided state_name
+    cursor.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC", (state_name,))
+
+    # Fetch all the rows in a list of tuples
+    results = cursor.fetchall()
+
+    # Print the results
+    for row in results:
+        print(row)
+
+    # Close the database connection
     db.close()
