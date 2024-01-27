7fc33252672554d28d967851b287359b329d4b28
sir <gari69wat@gmail.com>
2024-01-27 17:11:04 +0000

    function

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,3 +1,5 @@
+"""Module for checking if an object is an instance of, or inherited from, a specified class."""
+
 def is_kind_of_class(obj, a_class):
     """
     Check if an object is an instance of, or if the object is an instance of a class
