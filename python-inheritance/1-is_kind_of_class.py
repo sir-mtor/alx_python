115a2d1a8c89c9dc3dc9a8d61b81e8309a234ed9
sir <gari69wat@gmail.com>
2024-01-27 17:09:05 +0000

    kind

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,24 @@
+def is_kind_of_class(obj, a_class):
+    """
+    Check if an object is an instance of, or if the object is an instance of a class
+    that inherited from, the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if the object is an instance of, or if the object is an instance
+              of a class that inherited from, the specified class; otherwise, False.
+    """
+    return isinstance(obj, a_class)
+
+# Test cases
+if __name__ == "__main__":
+    a = 1
+    if is_kind_of_class(a, int):
+        print("{} comes from {}".format(a, int.__name__))
+    if is_kind_of_class(a, float):
+        print("{} comes from {}".format(a, float.__name__))
+    if is_kind_of_class(a, object):
+        print("{} comes from {}".format(a, object.__name__))
