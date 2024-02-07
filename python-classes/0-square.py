c6839d17bd7986463aa74d1cc00b4d7de972409b
sir <gari69wat@gmail.com>
2024-02-07 18:48:14 +0000

    sqr

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,20 @@
 class Square:
+     """
+    A class to represent a square.
+
+    Attributes:
+    __size (int): The size of the square.
+    """
     
 
-    def __init__(self, size):
+def __init__(self, size):
         self.__size = size
+        """
+        Initializes a Square object with a given size.
+
+        Parameters:
+        size (int): The size of the square.
+        """
 
 if __name__ == "__main__":
     my_square = Square(3)
