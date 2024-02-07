5a2a1d484093824837a56d0be580ca36dc504cf1
sir <gari69wat@gmail.com>
2024-02-07 17:45:06 +0000

    rec3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -95,6 +95,29 @@ if __name__ == "__main__":
         print("[{}] {}".format(e.__class__.__name__, e))
 
     def area(self):
-       
+        """ Compute and return the area of the rectangle """
         return self.width * self.height
-    
+
+if __name__ == "__main__":
+    try:
+        Rectangle(10, "2")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.width = -10
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        r = Rectangle(10, 2)
+        r.x = {}
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        Rectangle(10, 2, 3, -1)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+   
