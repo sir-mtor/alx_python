932a64f9ca2be4781977ee91ffb0f203867594e3
sir <gari69wat@gmail.com>
2024-02-08 12:20:27 +0000

    common

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -3,8 +3,9 @@ def common_elements(set_1, set_2):
     common_set = set_1 & set_2
     return common_set
 
-# Example usage:
+'''
 set_1 = {"Python", "C", "Javascript"}
 set_2 = {"Bash", "C", "Ruby", "Perl"}
 c_set = common_elements(set_1, set_2)
 print(sorted(list(c_set)))
+'''
