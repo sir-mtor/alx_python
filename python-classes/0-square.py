9efbf5bb4fe4fdc14d48cf70f55817ec649b27fa
sir <gari69wat@gmail.com>
2024-02-08 14:30:02 +0000

    0-squr

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,31 @@
 class Square:
+    def __init__(self, size):
+        self.__size = size
+
+    def __str__(self):
+        return "{} by {} by {} by {}".format(self.__size, self.__size, self.__size, self.__size)
+
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
+
+
+
+
+''''
+class Square:
 #This class defines a square by its size.
     
     def __init__(self, size):
@@ -26,3 +53,4 @@ square1.set_size(7)
 
 print("Updated Size:", square1.get_size())
 print("Updated Area:", square1.area())
+'''
