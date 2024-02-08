b229ec693c6d2e16081d3f031db6ad3d9e753908
sir <gari69wat@gmail.com>
2024-02-08 15:02:01 +0000

    cccc

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,27 @@
+"""
+This module defines a class Square that represents a square.
+
+The size of a square is crucial for various computations such as area computation. 
+To ensure control over the type and value of the size attribute, it is kept private.
+
+Example:
+    Square = __import__('0-square').Square
+
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square.__dict__)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
+"""
+
 class Square:
     """
     A class representing a square.
@@ -43,3 +67,4 @@ if __name__ == "__main__":
         print(my_square.__size)
     except Exception as e:
         print(e)
+
