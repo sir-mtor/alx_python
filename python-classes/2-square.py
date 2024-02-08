087cceabde59621801879060a7b2f614b35b2268
sir <gari69wat@gmail.com>
2024-02-08 15:13:54 +0000

    2-square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,20 +1,60 @@
+"""
+This module defines a class Square that represents a square.
+
+The size of a square is crucial for various computations such as area computation.
+To ensure control over the type and value of the size attribute, it is kept private.
+
+"""
+
 class Square:
+    """
+    A class representing a square.
+
+    Attributes:
+        __size (int): The size of the square.
+    """
+
     def __init__(self, size=0):
-        self.__size = 0  
-        self.size = size  
+        """
+        Initializes a Square object with the given size.
 
-    @property
-    def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
+        Parameters:
+            size (int): The size of the square. Default is 0.
+        
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = value
+            self.__size = size
 
     def area(self):
+        """
+        Computes and returns the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
         return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))
