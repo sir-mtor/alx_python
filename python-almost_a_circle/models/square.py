ce4977fa4991a76575496d902e1e91fef46fee2f
sir <gari69wat@gmail.com>
2024-02-15 19:58:43 +0000

    almost

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,21 +1,10 @@
+from models.rectangle import Rectangle
+"""Initializes a Square instance."""
+       
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position. Defaults to 0.
-            y (int, optional): The y-coordinate of the square's position. Defaults to 0.
-            id (int, optional): The id of the square. Defaults to None.
-        """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """
-        Returns a string representation of the Square instance.
-
-        Returns:
-            str: A string representing the Square instance.
-        """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
+
