a3ac94d2793a7e19ad485af0d52b98eef4824edf
sir <gari69wat@gmail.com>
2024-02-15 13:33:13 +0000

    almost

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,6 @@ class Rectangle(Base):
         """display rectangle"""
         for _ in range(self.height):
             print("#" * self.width)
+            
+    def __str__(self):
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
