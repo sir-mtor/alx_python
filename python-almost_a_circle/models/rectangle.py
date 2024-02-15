44537fa659e81ef9610b23049235cbc307b2059d
sir <gari69wat@gmail.com>
2024-02-15 19:44:37 +0000

    almost

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -110,25 +110,3 @@ class Rectangle(Base):
                 setattr(self, key, value)
 
                 from models.rectangle import Rectangle
-
-class Square(Rectangle):
-    def __init__(self, size, x=0, y=0, id=None):
-        """
-        Initializes a Square instance.
-
-        Args:
-            size (int): The size of the square.
-            x (int, optional): The x-coordinate of the square's position. Defaults to 0.
-            y (int, optional): The y-coordinate of the square's position. Defaults to 0.
-            id (int, optional): The id of the square. Defaults to None.
-        """
-        super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """
-        Returns a string representation of the Square instance.
-
-        Returns:
-            str: A string representing the Square instance.
-        """
-        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
